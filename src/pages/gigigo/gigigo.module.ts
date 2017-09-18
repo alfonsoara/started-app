d2b096e40f20d029c9506f2e758d874f02648e48
Alfonso Aranda <alfonso.aranda@MacBook-Pro-de-Alfonso.local>
2017-09-18 15:15:29 -0500

    started app

---
+++ src/pages/gigigo/gigigo.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { GigigoPage } from './gigigo';
+
+@NgModule({
+  declarations: [
+    GigigoPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(GigigoPage),
+  ],
+})
+export class GigigoPageModule {}
